9f15f5f6d6d56d072401de8d50c5ede553e3b92c
NebulousDev <nebulousdev@gmail.com>
2025-01-04 14:27:55 -0500

    Fixed button (again) to have a smoother transition

--- src/components/FadeButton.tsx
+++ src/components/FadeButton.tsx
@@ -10,8 +10,7 @@ export default function FadeButton({linkPath="/", width="w-full", height="h-full
     return (
         <Link to={linkPath} ref={ref1} className={`transition-opacity duration-500 ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
             <button className={`group/fade-button relative ${width} ${height} rounded-lg bg-slate-800 hover:font-medium hover:scale-[102%] transition-all duration-150 overflow-hidden pointer-events-auto ${extraClass}`}>
-                <div className={`absolute ${width} ${height} top-0 left-[-100%] opacity-20 scale-[101%] group-hover/fade-button:opacity-100 group-hover/fade-button:translate-x-[200%] duration-200 bg-gradient-to-r from-20% from-nebdev pointer-events-none`} />
-                <div className={`absolute ${width} ${height} top-0 left-[-200%] opacity-20 scale-[101%] group-hover/fade-button:opacity-100 group-hover/fade-button:translate-x-[200%] duration-200 bg-nebdev pointer-events-none`} />
+                <div className={`absolute ${width} ${height} top-0 left-[-100%] opacity-20 scale-y-[101%] scale-x-[201%] group-hover/fade-button:opacity-100 group-hover/fade-button:translate-x-[150%] duration-200 bg-gradient-to-r from-50% from-nebdev pointer-events-none`} />
                 <div className="relative">
                     {children}
                 </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,28 @@
 import { Link } from 'react-router-dom'
 import FadeButton from './FadeButton';
 import { SVGMenu } from './Icons';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import { useIsVisible } from './Visibility';
 
 function NavMenu()
 {
+    const ref1 : any = useRef();
+    const isVisible : boolean = useIsVisible(ref1);
+    
     return (
-        <div className="absolute top-[3.25rem] w-full h-fit bg-zinc-950">
+        <div ref={ref1} className={`absolute left-0 top-[3.25rem] sm:top-[3.55rem] w-full sm:w-[400px] h-fit bg-zinc-950 sm:bg-[#000000aa] sm:rounded-br-xl transition-all duration-150 ${isVisible ? 'opacity-100 translate-y-[0%]' : 'opacity-0 -translate-y-[100%]'}`}>
             <div className="basis-1/3 justify-self-center p-5 w-full">
                 <div className="w-full mx-auto grid grid-rows-3 h-full gap-2">
-                    <FadeButton extraClass="h-[3rem]" linkPath="/projects">
+                    <FadeButton extraClass="h-[3.0rem]" linkPath="/">
+                        <h1 className="text-base sm:text-xl text-white font-mono text-center h-full">Home</h1>
+                    </FadeButton>
+                    <FadeButton extraClass="h-[3.0rem]" linkPath="/projects">
                         <h1 className="text-base sm:text-xl text-white font-mono text-center h-full">Projects</h1>
                     </FadeButton>
-                    <FadeButton extraClass="h-[3rem]" linkPath="/blog">
+                    <FadeButton extraClass="h-[3.0rem]" linkPath="/blog">
                         <h1 className="text-base sm:text-xl text-white font-mono text-center h-full">Blog</h1>
                     </FadeButton>
-                    <FadeButton extraClass="h-[3rem]" linkPath="/contact">
+                    <FadeButton extraClass="h-[3.0rem]" linkPath="/contact">
                         <h1 className="text-base sm:text-xl text-white font-mono text-center h-full">Contact</h1>
                     </FadeButton>
                 </div>
@@ -68,15 +75,16 @@ export default function Header({isSticky=true, showBg=true, showLogo=true, showN
 
                     <div className='group order-first shrink-0 visible lg:hidden pointer-events-auto' onClick={toggleNavMenu}>
                         <SVGMenu className="fill-white hover:fill-nebdev pl-[5px] pr-[2px] py-[2px] w-[40px] sm:w-[40px] h-[40px] sm:h-[40px] mt-[-2px] sm:mt-0"/>
+
+                        { isNavMenuOpen &&
+                    
+                        <>
+                            <NavMenu />
+                        </>
+                        
+                        }
                     </div>
 
-                    {isNavMenuOpen &&
-                    
-                    <>
-                        <NavMenu />
-                    </>
-                    
-                    }
                 </>
             }
 
